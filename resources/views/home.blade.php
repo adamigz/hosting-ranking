837f5b0f23a4c83f0fbfa214d04ad903f9fac277
Adam Pitrowski <pitrowskiadamm@gmail.com>
2021-10-04 15:12:47 +0200

    Added icons to vote buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,13 +60,20 @@
                                         <th scope="row">{{ $index+1 }}</th>
                                         <td>
                                             @auth
-                                            <a href="{{ route('vote', $hosting->id) }}">
+                                            <a class="text-decoration-none" href="{{ route('vote', $hosting->id) }}">
                                                 @if(Auth::user()->votedOn($hosting->id))
-                                                <button class="btn btn-success">
+                                                <button class="btn d-flex btn-success">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-square-fill my-auto me-2" viewBox="0 0 16 16">
+                                                        <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+                                                    </svg>
                                                     {{ $hosting->votes_count }}
                                                 </button>
                                                 @else
-                                                <button class="btn btn-warning">
+                                                <button class="btn d-flex btn-warning">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-square my-auto me-2" viewBox="0 0 16 16">
+                                                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                                    </svg>
                                                     {{ $hosting->votes_count }}
                                                 </button>
                                                 @endif
@@ -74,7 +81,11 @@
                                             @endauth
                                             @guest
                                             <a href="{{ route('login') }}">
-                                                <button class="btn btn-warning">
+                                                <button class="btn d-flex btn-warning">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-square my-auto me-2" viewBox="0 0 16 16">
+                                                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                                    </svg>
                                                     <abbr title="Musisz się zalogować, aby móc głosować" class="initialism">{{ $hosting->votes_count }}</abbr>
                                                 </button>
                                             </a>
